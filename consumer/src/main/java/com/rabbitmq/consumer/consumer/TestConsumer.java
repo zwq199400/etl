7f63dda8d2310282f43d53ecd7f3185458c6e066
周伟强 <31526884+zwq199400@users.noreply.github.com>
2020-10-30 15:48:04 +0800

    feat: 消费者导出消息至es中

--- consumer/src/main/java/com/rabbitmq/consumer/consumer/TestConsumer.java
+++ consumer/src/main/java/com/rabbitmq/consumer/consumer/TestConsumer.java
@@ -3,8 +3,16 @@ package com.rabbitmq.consumer.consumer;
 import com.etl.entity.pojo.BaseEntity;
 import com.rabbitmq.consumer.binders.TestInputBinder;
 import com.rabbitmq.consumer.constant.TestBinderConstant;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+
+import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  * @author zhouwq
@@ -13,8 +21,20 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 @EnableBinding(TestInputBinder.class)
 public class TestConsumer {
 
+
+    @Resource
+    private RestHighLevelClient restHighLevelClient;
+
     @StreamListener(value = TestBinderConstant.TEST_QUEUE_INPUT)
-    public void testConsumer(BaseEntity message) {
+    public void testConsumer(BaseEntity message) throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        IndexRequest indexRequest = new IndexRequest();
+        indexRequest
+                .index("idx_user")
+                .setRefreshPolicy(WriteRequest.RefreshPolicy.NONE)
+                .source(message);
+        bulkRequest.add(indexRequest);
+        restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
         System.out.println(message.toString());
     }
 }
